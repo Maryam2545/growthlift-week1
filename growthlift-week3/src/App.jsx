8c3def57e4a73087f55a20e8cb35748423cb1144
Maryam <maryworld45@gmail.com>
2026-09-07 11:27:45 -0400

    Day 11: React setup and JSX basics

---
+++ growthlift-week3/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h2>React Setup & JSX Basics</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ growthlift-week3/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>GrowthLift Internship</h1>
+    </header>
+  );
+}
+
+export default Header;
